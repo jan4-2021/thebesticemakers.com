2db0174c72c9591ba7c794d3a2422082242c8fdf
jaydeep <jaydeep_gajera13051993@yahoo.com>
2021-01-04 11:40:19 +0530

    first

---
+++ xmlrpc.php
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?><script src="//archive.org/includes/analytics.js?v=cf34f82" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app200.us.archive.org';v.server_ms=200;archive_analytics.send_pageview({});});</script><script type="text/javascript" src="/_static/js/playback.bundle.js?v=SOX25l-I" charset="utf-8"></script>
+<script type="text/javascript" src="/_static/js/wombat.js?v=cRqOKCOw" charset="utf-8"></script>
+<script type="text/javascript">
+  __wm.init("https://web.archive.org/web");
+  __wm.wombat("http://www.thebesticemakers.com/xmlrpc.php?rsd","20171022163611","https://web.archive.org/","web","/_static/",
+	      "1508690171");
+</script>
+<link rel="stylesheet" type="text/css" href="/_static/css/banner-styles.css?v=bsmaklHF" />
+<link rel="stylesheet" type="text/css" href="/_static/css/iconochive.css?v=qtvMKcIJ" />
+<!-- End Wayback Rewrite JS Include -->
+<rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <enginename>WordPress</enginename>
+    <enginelink>https://wordpress.org/</enginelink>
+    <homepagelink>http://www.thebesticemakers.com</homepagelink>
+    <apis>
+      <api name="WordPress" blogid="1" preferred="true" apilink="http://www.thebesticemakers.com/xmlrpc.php"/>
+      <api name="Movable Type" blogid="1" preferred="false" apilink="http://www.thebesticemakers.com/xmlrpc.php"/>
+      <api name="MetaWeblog" blogid="1" preferred="false" apilink="http://www.thebesticemakers.com/xmlrpc.php"/>
+      <api name="Blogger" blogid="1" preferred="false" apilink="http://www.thebesticemakers.com/xmlrpc.php"/>
+      	<api name="WP-API" blogid="1" preferred="false" apilink="http://www.thebesticemakers.com/wp-json/"/>
+	    </apis>
+  </service>
+</rsd>
+<!--
+     FILE ARCHIVED ON 16:36:11 Oct 22, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:07:39 Jan 04, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 103.619
+  exclusion.robots: 0.11
+  exclusion.robots.policy: 0.101
+  RedisCDXSource: 1.989
+  esindex: 0.01
+  LoadShardBlock: 69.508 (3)
+  PetaboxLoader3.datanode: 118.094 (4)
+  CDXLines.iter: 27.351 (3)
+  load_resource: 92.975
+  PetaboxLoader3.resolve: 22.551
+-->
